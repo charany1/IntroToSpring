0dbae3a14787e042399ec2865062689b15e3cc94
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-14 10:03:10 +0530

    Creates Car and use that in Application

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+
+    public static void main(String[] args) {
+
+        //Car is there on Type declaration side and on Object instantiation side
+        Car obj = new Car();
+        obj.drive();
+    }
+
+
+
+}
